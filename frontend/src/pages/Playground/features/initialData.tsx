2e06f9ef7de21608cf7419a6c15d73266e2d1742
Piyush Padia <piyush.padia2021@vitstudent.ac.in>
2025-04-26 13:54:07 +0530

    changes

--- frontend/src/pages/Playground/features/initialData.tsx
+++ frontend/src/pages/Playground/features/initialData.tsx
@@ -1,147 +1,157 @@
 interface Point {
-    x: number;
-    z: number;
-  }
-  
-  interface Room {
-    id: string;
-    room_type: string;
-    area: number;
-    height: number;
-    width: number;
-    floor_polygon: Point[];
-  }
-  
-  interface FloorPlanData {
-    room_count: number;
-    total_area: number;
-    room_types: string[];
-    rooms: Room[];
-  }
-  
-  export const initialFloorPlanData: FloorPlanData = {
-    room_count: 8,
-    total_area: 62.57,
-    room_types: [
-        "MasterRoom",
-        "LivingRoom",
-        "ChildRoom",
-        "Kitchen",
-        "Bathroom",
-        "Balcony",
-        "Balcony",
-        "Balcony"
-    ],
-    rooms: [
-      {
-        id: "room|0",
-        room_type: "MasterRoom",
-        area: 10.01,
-        height: 2.88,
-        width: 3.52,
-        floor_polygon: [
-          { x: 141, z: 75 },
-          { x: 191, z: 75 },
-          { x: 191, z: 116 },
-          { x: 141, z: 116 },
-        ],
-      },
-      {
-        id: "room|1",
-        room_type: "LivingRoom",
-        area: 25.14,
-        height: 5.48,
-        width: 8.86,
-        floor_polygon: [
-          { x: 65, z: 75 },
-          { x: 137, z: 75 },
-          { x: 137, z: 122 },
-          { x: 191, z: 122 },
-          { x: 191, z: 139 },
-          { x: 141, z: 139 },
-          { x: 141, z: 153 },
-          { x: 123, z: 153 },
-          { x: 123, z: 139 },
-          { x: 65, z: 139 },
-        ],
-      },
-      {
-        id: "room|2",
-        room_type: "ChildRoom",
-        area: 8.31,
-        height: 2.67,
-        width: 3.23,
-        floor_polygon: [
-          { x: 145, z: 143 },
-          { x: 191, z: 143 },
-          { x: 191, z: 181 },
-          { x: 145, z: 181 },
-        ],
-      },
-      {
-        id: "room|3",
-        room_type: "Kitchen",
-        area: 4.75,
-        height: 1.27,
-        width: 3.8,
-        floor_polygon: [
-          { x: 65, z: 143 },
-          { x: 119, z: 143 },
-          { x: 119, z: 161 },
-          { x: 65, z: 161 },
-        ],
-      },
-      {
-        id: "room|4",
-        room_type: "Bathroom",
-        area: 2.37,
-        height: 1.69,
-        width: 1.27,
-        floor_polygon: [
-          { x: 123, z: 157 },
-          { x: 141, z: 157 },
-          { x: 141, z: 181 },
-          { x: 123, z: 181 },
-        ],
-      },
-      {
-        id: "room|5",
-        room_type: "Balcony",
-        area: 3.11,
-        height: 0.98,
-        width: 3.8,
-        floor_polygon: [
-          { x: 65, z: 55 },
-          { x: 119, z: 55 },
-          { x: 119, z: 69 },
-          { x: 65, z: 69 },
-        ],
-      },
-      {
-        id: "room|6",
-        room_type: "Balcony",
-        area: 3.11,
-        height: 1.12,
-        width: 3.8,
-        floor_polygon: [
-          { x: 65, z: 165 },
-          { x: 119, z: 165 },
-          { x: 119, z: 181 },
-          { x: 65, z: 181 },
-        ],
-      },
-      {
-        id: "room|7",
-        room_type: "Balcony",
-        area: 5.76,
-        height: 1.12,
-        width: 3.8,
-        floor_polygon: [
-          { x: 145, z: 185 },
-          { x: 191, z: 185 },
-          { x: 191, z: 197 },
-          { x: 145, z: 197 },
-        ],
-      },
-    ],
-  };
+  x: number;
+  z: number;
+}
+
+interface Room {
+  id: string;
+  room_type: string;
+  area: number;
+  height: number;
+  width: number;
+  floor_polygon: Point[];
+  is_regular?: number;
+}
+
+interface FloorPlanData {
+  room_count: number;
+  total_area: number;
+  room_types: string[];
+  rooms: Room[];
+}
+
+// Scale factor - calculated from ratio of old to new coordinate ranges
+const SCALE_FACTOR = 20; 
+
+export const initialFloorPlanData: FloorPlanData = {
+  "room_count": 8,
+  "total_area": 70.95,
+  "room_types": [
+    "LivingRoom",
+    "Kitchen",
+    "Bathroom",
+    "MasterRoom",
+    "SecondRoom",
+    "Balcony",
+    "Balcony",
+    "Balcony"
+  ],
+  "rooms": [
+    {
+      "area": 12.01,
+      "floor_polygon": [
+        {"x": 3.3 * SCALE_FACTOR, "z": 3.3 * SCALE_FACTOR},
+        {"x": 3.3 * SCALE_FACTOR, "z": 6.6 * SCALE_FACTOR},
+        {"x": 8.2 * SCALE_FACTOR, "z": 6.6 * SCALE_FACTOR},
+        {"x": 8.2 * SCALE_FACTOR, "z": 5.5 * SCALE_FACTOR},
+        {"x": 6.6 * SCALE_FACTOR, "z": 5.5 * SCALE_FACTOR},
+        {"x": 6.6 * SCALE_FACTOR, "z": 3.3 * SCALE_FACTOR}
+      ],
+      "height": 3.3,
+      "id": "room|2",
+      "is_regular": 0,
+      "room_type": "LivingRoom",
+      "width": 4.9
+    },
+    {
+      "area": 4.75,
+      "floor_polygon": [
+        {"x": 3.3 * SCALE_FACTOR, "z": 1.6 * SCALE_FACTOR},
+        {"x": 3.3 * SCALE_FACTOR, "z": 3.3 * SCALE_FACTOR},
+        {"x": 6.6 * SCALE_FACTOR, "z": 3.3 * SCALE_FACTOR},
+        {"x": 6.6 * SCALE_FACTOR, "z": 1.6 * SCALE_FACTOR}
+      ],
+      "height": 1.6,
+      "id": "room|3",
+      "is_regular": 0,
+      "room_type": "Kitchen",
+      "width": 3.3
+    },
+    {
+      "area": 3.32,
+      "floor_polygon": [
+        {"x": 0.0 * SCALE_FACTOR, "z": 5.5 * SCALE_FACTOR},
+        {"x": 0.0 * SCALE_FACTOR, "z": 7.1 * SCALE_FACTOR},
+        {"x": 1.6 * SCALE_FACTOR, "z": 7.1 * SCALE_FACTOR},
+        {"x": 1.6 * SCALE_FACTOR, "z": 5.5 * SCALE_FACTOR}
+      ],
+      "height": 2.2,
+      "id": "room|4",
+      "is_regular": 0,
+      "room_type": "Bathroom",
+      "width": 1.6
+    },
+    {
+      "area": 12.01,
+      "floor_polygon": [
+        {"x": 0.0 * SCALE_FACTOR, "z": 1.6 * SCALE_FACTOR},
+        {"x": 0.0 * SCALE_FACTOR, "z": 3.3 * SCALE_FACTOR},
+        {"x": 1.6 * SCALE_FACTOR, "z": 3.3 * SCALE_FACTOR},
+        {"x": 1.6 * SCALE_FACTOR, "z": 4.9 * SCALE_FACTOR},
+        {"x": 3.3 * SCALE_FACTOR, "z": 4.9 * SCALE_FACTOR},
+        {"x": 3.3 * SCALE_FACTOR, "z": 1.6 * SCALE_FACTOR}
+      ],
+      "height": 3.3,
+      "id": "room|5",
+      "is_regular": 0,
+      "room_type": "MasterRoom",
+      "width": 3.3
+    },
+    {
+      "area": 10.01,
+      "floor_polygon": [
+        {"x": 3.3 * SCALE_FACTOR, "z": 0.0 * SCALE_FACTOR},
+        {"x": 3.3 * SCALE_FACTOR, "z": 1.6 * SCALE_FACTOR},
+        {"x": 6.6 * SCALE_FACTOR, "z": 1.6 * SCALE_FACTOR},
+        {"x": 6.6 * SCALE_FACTOR, "z": 0.0 * SCALE_FACTOR}
+      ],
+      "height": 1.6,
+      "id": "room|6",
+      "is_regular": 1,
+      "room_type": "SecondRoom",
+      "width": 3.3
+    },
+    {
+      "area": 3.56,
+      "floor_polygon": [
+        {"x": 0.0 * SCALE_FACTOR, "z": 0.0 * SCALE_FACTOR},
+        {"x": 0.0 * SCALE_FACTOR, "z": 1.6 * SCALE_FACTOR},
+        {"x": 2.0 * SCALE_FACTOR, "z": 1.6 * SCALE_FACTOR},
+        {"x": 2.0 * SCALE_FACTOR, "z": 0.0 * SCALE_FACTOR}
+      ],
+      "height": 1.6,
+      "id": "room|7",
+      "is_regular": 0,
+      "room_type": "Balcony",
+      "width": 2.0
+    },
+    {
+      "area": 2.91,
+      "floor_polygon": [
+        {"x": 6.6 * SCALE_FACTOR, "z": 0.0 * SCALE_FACTOR},
+        {"x": 6.6 * SCALE_FACTOR, "z": 1.6 * SCALE_FACTOR},
+        {"x": 8.2 * SCALE_FACTOR, "z": 1.6 * SCALE_FACTOR},
+        {"x": 8.2 * SCALE_FACTOR, "z": 0.0 * SCALE_FACTOR}
+      ],
+      "height": 1.6,
+      "id": "room|8",
+      "is_regular": 0,
+      "room_type": "Balcony",
+      "width": 1.6
+    },
+    {
+      "area": 2.91,
+      "floor_polygon": [
+        {"x": 1.6 * SCALE_FACTOR, "z": 5.5 * SCALE_FACTOR},
+        {"x": 1.6 * SCALE_FACTOR, "z": 7.1 * SCALE_FACTOR},
+        {"x": 3.3 * SCALE_FACTOR, "z": 7.1 * SCALE_FACTOR},
+        {"x": 3.3 * SCALE_FACTOR, "z": 5.5 * SCALE_FACTOR}
+      ],
+      "height": 1.6,
+      "id": "room|9",
+      "is_regular": 0,
+      "room_type": "Balcony",
+      "width": 1.7
+    }
+  ]
+};
